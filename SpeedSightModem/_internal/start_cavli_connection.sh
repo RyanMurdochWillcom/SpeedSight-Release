c6433752237e2fb88281963a954f3da898536742
Ryan Murdoch <rmurdoch@willcom.co.za>
2024-08-27 10:52:48 +0200

    Update SpeedSightModem

--- SpeedSightModem/_internal/start_cavli_connection.sh
+++ SpeedSightModem/_internal/start_cavli_connection.sh
@@ -3,5 +3,4 @@ cd /Apps/SpeedSight-Release/SpeedSightModem/_internal
 rm connectionLog.log
 bash ./cavli_c20qm.sh > connectionLog.log
 echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
-cd /Apps/SpeedSight-Release/SpeedSightModem
 echo '{"modemmodel":"cavli_c20qm"}' > modemmodel.json
--- SpeedSightModem/_internal/start_meig_connection.sh
+++ SpeedSightModem/_internal/start_meig_connection.sh
@@ -6,5 +6,4 @@ echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
 ifmetric ppp0 50
 ifmetric wlan0 100
 ifmetric eth0 150
-cd /Apps/SpeedSight-Release/SpeedSightModem
 echo '{"modemmodel":"slm750"}' > modemmodel.json
